e17055d81ebb4f5f12f39587489f8511daef5dba
Sio <sioclya@mailbox.tu-berlin.de>
2018-05-02 19:18:56 +0200

    removed unnecessary clutter from caml_src/step.ml

--- caml_src/step.ml
+++ caml_src/step.ml
@@ -1,19 +1,5 @@
 (* Copyright (C) 2017 Sio Kreuzer. All Rights Reserved. *)
 
-(*
-TODO:
-    - make an object to store game's state
-    - declare object on global scope as mutable in some way
-    - go through S.T.A.L.K.E.R. SoC wiki and make sure we can replicate described gameplay from definitions in files/SQLite3 dbs
-    - make the game's state storable/loadable to/from an SQLite3 db
-*)
-
-(*
-let game_state = ref GameState;; (* TODO: need one of these types'a objects. *)
-
-NOTE: it should be possible to have step take a GameState and return another one to C++ code.
-*)
-
 let init () =
     Printf.printf "Initializing Game module...\n";
     flush stdout;;
